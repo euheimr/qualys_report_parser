292430b312feccd2f0b94a3a03f785d29ea3bf88
Jake <euhe@pm.me>
2020-11-23 01:36:57 -0800

    Simplified the function calls

--- main.py
+++ main.py
@@ -1,46 +1,26 @@
 
 __author__ = 'Jacob Betz'
+__email__ = 'euhe@pm.me'
 
-import file_utils as util
 from loguru import logger as log
+
+''' Local imports '''
+import utils
+#import config
 
 _write_mode = 'print'
 
 
 def main():
+    #try:
+    #    config.read_config()
+    #except Exception as ex:
+    #    log.exception(f'Could not read config: {str(ex)}')
+
     try:
-        ''' get_xml_file_paths iterates over a directory and returns a list of 
-            file-paths '''
-        for file in util.get_xml_file_paths():
-            ''' Test print to see the value of _file_list '''
-            log.debug(f'File list, path: {str(file)}')
-
-            try:
-                ''' parse_xml returns a Pandas Dataframe '''
-                df = util.parse_xml(file_list=file)
-
-                try:
-                    ''' Determine the write mode and write out to file if given 
-                        the option '''
-                    status = util.write_file()
-
-                    if status:
-                        log.debug(f'Successfully wrote file using the '
-                                  f'write_mode: {str(_write_mode)}')
-
-                    elif status is False:
-                        log.error(f'[main] Failed to write to file: '
-                                  f'{str(file)}')
-
-                except Exception as ex:
-                    log.debug(f'[main] Failed to parse XML: {str(ex)}')
-                    return False
-
-            except Exception as ex:
-                log.debug(f'[main] Could not parse XML: {str(ex)}')
-
+        utils.parse_reports()
     except Exception as ex:
-        log.debug(f'[main] Could not parse file path(s): {str(ex)}')
+        log.exception(f'Could not generate reports: {str(ex)}')
 
 
 if __name__ == '__main__':
